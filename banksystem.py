f789ee42a9987ff566e7759a39640b6c0c66059d
KeerthiKongari <keerthikongari26@gmail.com>
2020-07-03 13:14:19 +0530

    second stage complete

--- banksystem.py
+++ banksystem.py
@@ -1,10 +1,52 @@
 import random
 
+def fun():
+    while True:
+        val = random.randint(4000000000000000, 4000009999999999)
+        y = list(str(val))
+        total = 0
+        for num in range(0, 15):
+            if num % 2 == 0:
+                x = int(y[num]) * 2
+                y[num] = str(x)
+        for num in range(0, 15):
+            if int(y[num]) > 9:
+                x = int(y[num]) - 9
+                y[num] = str(x)
+        for num in range(0, 15 + 1):
+            total += int(y[num])
+            y[num] = int(y[num])
+        if total % 10 == 0:
+            return val
+            break
+        else:
+            continue
+
+def fun_verify(val):
+    v = val
+    y = list(str(v))
+    total = 0
+    for num in range(0, 15):
+        if num % 2 == 0:
+            x = int(y[num]) * 2
+            y[num] = str(x)
+    for num in range(0, 15):
+        if int(y[num]) > 9:
+            x = int(y[num]) - 9
+            y[num] = str(x)
+    for num in range(0, 15 + 1):
+        total += int(y[num])
+        y[num] = int(y[num])
+    if total % 10 == 0:
+        return "yes"
+    else:
+        return "no"
 
 class CreditCard:
-
+    accout_number = 0
+    pin = 0
     def __init__(self):
-        self.account_number = f"{random.randint(4000000000000000, 4000009999999999):16}"
+        self.account_number = fun()
         self.pin = f"{random.randint(0000, 9999):04}"
         self.balance = 0
 
@@ -27,7 +69,8 @@ Your card PIN:
     elif choice == "2":
         card_number = input("Enter your card number:")
         pin_number = input("Enter your PIN:")
-        if card_number != credit_card.account_number or pin_number != credit_card.pin:
+        res = fun_verify(card_number)
+        if res == "no" and card_number != credit_card.account_number or pin_number != credit_card.pin:
             print("Wrong card number or PIN!")
         else:
             print("You have successfully logged in!")
