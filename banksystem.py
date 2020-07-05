c33c5df6fdd319a58b9be6e45f0d7095acaa771b
KeerthiKongari <keerthikongari26@gmail.com>
2020-07-05 16:46:45 +0530

    3/4 stage

--- banksystem.py
+++ banksystem.py
@@ -1,6 +1,12 @@
 import random
-
-def Lunh_algo():
+import sqlite3
+conn = sqlite3.connect('card.s3db')
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS card (id INTEGER, number TEXT, pin TEXT, balance INTEGER DEFAULT 0)""")
+ 
+conn.commit()
+ 
+def fun():
     while True:
         val = random.randint(4000000000000000, 4000009999999999)
         y = list(str(val))
@@ -21,8 +27,8 @@ def Lunh_algo():
             break
         else:
             continue
-
-def card_verify(val):
+ 
+def fun_verify(val):
     v = val
     y = list(str(v))
     total = 0
@@ -41,35 +47,40 @@ def card_verify(val):
         return "yes"
     else:
         return "no"
-
+ 
 class CreditCard:
     accout_number = 0
     pin = 0
     def __init__(self):
-        self.account_number = Lunh_algo()
+        self.account_number = fun()
         self.pin = f"{random.randint(0000, 9999):04}"
         self.balance = 0
-
+ 
     def __repr__(self):
         return f"{self.account_number}"
-
-
+ 
+ 
 while True:
     choice = input("1. Create an account \n"
                    "2. Log into account \n"
                    "0. Exit")
-
+ 
     if choice == "1":
         credit_card = CreditCard()
+        card_num = credit_card.account_number
+        credit_pin = credit_card.pin
+        cur.execute(f'''INSERT INTO card (number,pin)
+                VALUES ({card_num},{credit_pin})''')
+        conn.commit()
         print(f"""Your card has been created
 Your card number:
-{credit_card.account_number}
+{card_num}
 Your card PIN:
-{credit_card.pin}""")
+{credit_pin}""")
     elif choice == "2":
         card_number = input("Enter your card number:")
         pin_number = input("Enter your PIN:")
-        res = card_verify(card_number)
+        res = fun_verify(card_number)
         if res == "no" and card_number != credit_card.account_number or pin_number != credit_card.pin:
             print("Wrong card number or PIN!")
         else:
@@ -88,3 +99,4 @@ Your card PIN:
     else:
         print("Bye!")
         break
+conn.close()
